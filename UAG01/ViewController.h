153bf7463fdda78d258bec31b0af2468b909efd8
Walter <walter.gonzalez@smartplace.mx>
2014-09-27 12:42:19 -0500

    Initial Commit

---
+++ UAG01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UAG01
+//
+//  Created by Walter on 27/09/14.
+//  Copyright (c) 2014 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UAG01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UAG01
+//
+//  Created by Walter on 27/09/14.
+//  Copyright (c) 2014 Smartplace. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
